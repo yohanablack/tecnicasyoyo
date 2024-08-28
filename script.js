5bfa6da7cb6cc0001ddf55466261b3c6692a5877
Yohana <117214321+yohanablack@users.noreply.github.com>
2024-08-28 10:14:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu da escola você se depara com um robo IA, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: ["No início ficou com medo do que essa tecnologia pode fazer. ",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                texto: "Isso é medonho!",
+                afirmacao: ["No início ficou com medo do que esse robo IA pode fazer. ",
+                    "Achou assustador pensar na velocidade na qual a nossa tecnologia está avançando."
                 ]
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Isso é esplendido!",
                 afirmacao: ["Quis saber como usar IA no seu dia a dia.",
                     "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia."
                 ]
